ce459c6cbc71e5a14cfe2ccc38d985273698b6a3
cmcfarland <mcfarland.c.m@gmail.com>
2023-11-21 23:54:49 -0500

    split into helper classes: working!

--- 04-audio-visualizer/pages/index.jsx
+++ 04-audio-visualizer/pages/index.jsx
@@ -24,27 +24,16 @@ export default function Home() {
   return (
     <div id="mainDiv" className="flex flex-col items-center justify-center">
       <div id="mediaDiv" className="absolute bottom-2 right-2">
-        <video id="media" controls autoPlay 
-          // live RttP audiostream working!! live visuals, not so much
-          
+        {/* <video id="media" controls autoPlay crossorigin="anonymous" >
           // MediaElementAudioSource outputs zeroes due to CORS access restrictions for <url> [Chrome]
-          // The HTMLMediaElement passed to createMediaElementSource has a cross-origin resource, the node will output silence. [Firefox]
           // https://stackoverflow.com/questions/48362093/cors-request-blocked-in-locally-opened-html-file 
 
           // try running HTTPS server to get around CORS errors: 
           // https://create-react-app.dev/docs/using-https-in-development/
           // https://github.com/vercel/next.js/issues/8945#issuecomment-997267328
-        >
-          {/* 
-            src='https://labs.phaser.io/assets/audio/Dafunk - Hardcore Power (We Believe In Goa - Remix).ogg' type="audio/ogg"
-            src='https://nstmradio.mixlr.com/events/2787207' type="audio/mpeg"
-            src='https://www.youtube.com/live/MwtVkPKx3RA;' type="audio/mpeg"
           */}
-          <source src="http://s9.viastreaming.net:9000/;stream.mp3" 
-                  type="audio/mpeg"/> 
-        </video>
       </div>
-      <canvas id="threeJsCanvas"></canvas>
+      <canvas id='threeJsCanvas'></canvas>
     </div>
   );
 }
--- 04-audio-visualizer/pages/lib/SceneInit.js
+++ 04-audio-visualizer/pages/lib/SceneInit.js
@@ -7,20 +7,41 @@ import Stats from "three/examples/jsm/libs/stats.module";
 import Oscilloscope from "./Oscilloscope.js"
 import { vertexShaderScope, fragmentShaderScope } from "./Oscilloscope";
 
-// https://medium.com/@coderfromnineteen/three-js-post-processing-outline-effect-6dff6a2fe3c0
+var mediaLinks = {
+  0: { src:'../00_ice_sheets.mp3', 
+       type:'audio/mpeg' },
+  1: { src:"https://labs.phaser.io/assets/audio/Dafunk - Hardcore Power (We Believe In Goa - Remix).ogg", 
+       type:'audio/ogg' },
+  2: { src:'https://www.youtube.com/live/MwtVkPKx3RA;', 
+       type:'audio/mpeg' },
+  3: { src:'http://s9.viastreaming.net:9000/;stream.mp3', 
+       type:'audio/mpeg' },
+  4: { src:'https://nstmradio.mixlr.com/events/2787207', 
+       type:'audio/mpeg' }
+};
+
+{/*// https://medium.com/@coderfromnineteen/three-js-post-processing-outline-effect-6dff6a2fe3c0
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
 
 // const dat = dynamic(() => import("dat.gui"), {
 //   ssr: false, // Do not import in server side
 // })
+*/}
+// set multiple object attributes at once
+const setMultiAttributes = (object, attributes) => {
+  try { 
+    // https://stackoverflow.com/questions/22191576/javascript-createelement-and-setattribute
+    console.log(Object.entries(attributes));
+    Object.entries(attributes).forEach(([key, val]) => object.setAttribute(key, val));
+  } catch (error) { console.error(error); }
+}
 
 export default class SceneInit {
   constructor(canvasID='threeJsCanvas', fftSize=2048, fov=36) {
     this.canvasID = canvasID;
     this.fov = fov;
     this.fftSize = fftSize;
-
     this.dataArray = new Uint8Array(fftSize / 2);
     this.timeArray = new Float32Array(fftSize / 2);
     this.clock = new THREE.Clock();
@@ -33,80 +54,93 @@ export default class SceneInit {
       canvas: this.canvas,
       antialias: true,
     });
-    
+    this.renderer.domElement.id = "renderer";
+
     this.controls = new OrbitControls(this.camera, this.renderer.domElement); // this.render.domElement
-  }
+
+    console.log(mediaLinks);
+
+    document.head.title = 'Livestream music visualizer';
+    this.mainDiv = document.getElementById('mainDiv');
+    this.mediaDiv = document.getElementById('mediaDiv');
+    this.eventLog = document.querySelector(".event-log-contents");
+    if (this.media === undefined) {
+      this.media = new MediaInit();
+      // remove old media elements and add current one
+      while (this.mediaDiv.firstChild) {
+        this.mediaDiv.removeChild(this.mediaDiv.firstChild);
+      }
+      this.mediaDiv.appendChild(this.media.video);
+    }
+}
   
   setupScene() {
-    const setupAudioContext = () => {
-      // bug when switching betwen tracks in Chrome (needs refresh and/or browser restart): 
-      // InvalidStateError: Failed to execute 'createMediaElementSource' on 'AudioContext': 
-      // HTMLMediaElement already connected previously to a different MediaElementSourceNode.
-      // https://stackoverflow.com/questions/50657659/different-behaviour-of-webaudio-api-on-google-chrome  
-      this.audioContext = new window.AudioContext();
-      this.audioElement = document.getElementById("media");
-      // this.audioElement.onplay = {playMedia};
-      this.audioSource = this.audioContext.createMediaElementSource(this.audioElement);
-      this.analyser = this.audioContext.createAnalyser();
-      this.analyser.fftSize = this.fftSize;
-  
-      // adjust playback volume AFTER analyser node to avoid affecting signal display
-      this.gainNode = new GainNode(this.audioContext, { gain: 0.5 });
-  
-      this.audioSource.connect(this.analyser);
-      this.analyser.connect(this.audioContext.destination);
-      console.log("media channels: " + this.analyser.channelCount);
-    };
-  
-    const setupGui = async () => {
-      const dat = await import("dat.gui");
 
-      // remove duplicate components caused by rapid-refresh 
-      const old_gui = document.getElementById('gui');
-      if (old_gui) {
-        old_gui.parentNode.removeChild(old_gui)
-      }
-  
-      this.gui = new dat.GUI();
-      const guiParentNode = this.gui.domElement.parentNode;
-      guiParentNode.id = 'gui'; 
-      document.body.appendChild(guiParentNode);
-  
-      const freqGui = this.gui.addFolder("frequency");
-      freqGui
-        .add(this.planeCustomMaterial, "wireframe")
-        .name("wireframe")
-        .setValue(true)
-        .listen();
-      freqGui
-        .add(this.uniforms_fft.u_amplitude, "value", 1.0, 25.0)
-        .name("freq scale")
-        .setValue(this.uniforms_fft.u_amplitude.value)
-        .listen();
-      // freqGui
-      //   .add(this.analyser, "smoothingTimeConstant", 0, 1.0)
-      //   .name("time smoothing")
-      //   .setValue(0.8)
-      //   .listen();
-      const audioGui = this.gui.addFolder("audio");
-      audioGui
-        .add(this.uniforms_scope.u_scale_y, "value", 10.0, 200.0)
-        .name("time scale: Y")
-        .setValue(this.uniforms_scope.u_scale_y.value)
-        .listen();
-      audioGui
-        .add(this.uniforms_scope.u_scale_x, "value", 1.0, 5.0)
-        .name("time scale: X")
-        .setValue(this.uniforms_scope.u_scale_x.value)
-        .listen();
-      audioGui
-        .add(this.uniforms_scope.u_gain, "value", 0.0, 1.0)
-        .name("gain")
-        .setValue(0.1)
-        .listen()
-        .onChange((new_gain) => {
-          this.gainNode.gain.setValueAtTime(new_gain, this.audioContext.currentTime)
-        });
+    const setupGui = async () => {
+      // try { // setup GUI parent element
+        const dat = await import("dat.gui");
+        try { // remove duplicate controls caused by rapid-refresh 
+          const old_gui = document.getElementById('gui');
+          if (old_gui) {
+            while (old_gui.firstChild) {
+              old_gui.removeChild(old_gui.firstChild);
+            }
+          }
+        } catch (error) { console.error(error); }
+        
+        try { // setup GUI child element
+          this.gui = new dat.GUI();
+          this.gui.domElement.id = 'scene_gui';
+          const guiParentNode = this.gui.domElement.parentNode;
+          guiParentNode.id = 'gui'; 
+          this.mainDiv.appendChild(guiParentNode);
+        } catch (error) { console.error(error); }
+
+        // setup frequency controls
+        try { 
+          this.freqGui = this.gui.addFolder("frequency");
+          this.freqGui
+            .add(this.planeCustomMaterial, "wireframe")
+            .name("wireframe")
+            .setValue(true)
+            .listen();
+          this.freqGui
+            .add(this.uniforms_fft.u_amplitude, "value", 1.0, 25.0)
+            .name("freq scale")
+            .setValue(this.uniforms_fft.u_amplitude.value)
+            .listen();
+          // freqGui
+          //   .add(this.media.analyser, "smoothingTimeConstant", 0, 1.0)
+          //   .name("time smoothing")
+          //   .setValue(0.8)
+          //   .listen();
+        } catch (error) { console.error(error); }
+        
+        // setup audio controls
+        try { 
+          this.audioGui = this.gui.addFolder("audio");
+          this.audioGui
+            .add(this.uniforms_scope.u_scale_y, "value", 10.0, 200.0)
+            .name("time scale: Y")
+            .setValue(this.uniforms_scope.u_scale_y.value)
+            .listen();
+          this.audioGui
+            .add(this.uniforms_scope.u_scale_x, "value", 1.0, 5.0)
+            .name("time scale: X")
+            .setValue(this.uniforms_scope.u_scale_x.value)
+            .listen();
+          this.audioGui
+            .add(this.uniforms_scope.u_gain, "value", 0.0, 1.0)
+            .name("volume")
+            .setValue(0.1)
+            .listen()
+            .onChange((new_gain) => {
+              this.media.gainVolume.gain.setValueAtTime(
+                new_gain, this.media.context.currentTime)
+            });
+        } catch (error) { console.error(error); }
+        // } catch (error) { console.error(error); }
+      // } catch (error) { console.error(error); }
     };
   
     const setupGeometry = () => {
@@ -222,62 +256,112 @@ export default class SceneInit {
       this.scene.add(this.planeMesh);
     };
 
-    const mainDiv = document.getElementById('mainDiv');
-    const canvas = document.getElementById(this.canvasID);
-    if (canvas) {
-      this.canvas = canvas;
-    }
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);  
+    // browser waits for gesture from user before initializing audio context
+    const updateMedia = () => {
+      // this.media.video.update();
+      if (this.media.context === undefined) {
+        this.media.setupAudioContext();
+        // can't add fft properties to GUI until context is created
+        this.freqGui
+          .add(this.media.analyser, "smoothingTimeConstant", 0, 1.0)
+          .name("time smoothing")
+          .setValue(0.8)
+          .listen();
+      }
+      const render = () => {
+        if (!this.media.video.paused) {
+          this.media.analyser.getByteFrequencyData(this.dataArray);
+          this.media.analyser.getFloatTimeDomainData(this.timeArray);
+      
+          // this.uniforms_fft.u_time.value = time;
+          this.uniforms_fft.u_data_arr.value = this.dataArray;
+          this.uniforms_scope.u_data_arr.value = this.timeArray;
+        }  
+        // note: call render function on every animation frame
+        requestAnimationFrame(render);
+      };
 
-    // position camera directly above origin
-    this.camera.position.z = 150;
-    // point camera towards origin (see also: camera.lookat)
-    this.controls.target = new THREE.Vector3(0, 0, 0);
+      render();
+    };
 
-    const stats = document.getElementById('stats');
-    if (stats) {
-      stats.parentNode.removeChild(stats)
-    }
-    this.stats = Stats();
-    this.stats.dom.id = 'stats';
-    mainDiv.appendChild(this.stats.dom);
+    
+    const setupDomElements = () => {
 
-    const scope = document.getElementById('scope');
-    if (scope) {
-      scope.parentNode.removeChild(scope)
-    }
-    this.scope = new Oscilloscope();
-    this.scope.dom.id = 'scope';
-    mainDiv.appendChild(this.scope.dom);
+      const handleEvent = (event) => {
+        if (event.type === 'play') { 
+          updateMedia();
+        } 
+        if (event.type === 'pause' || event.type === 'play' ) { 
+          console.log(`${event.type} => ${this.media.video.currentTime} / ${this.media.video.duration} sec`);
+        } else {
+          console.log(`${event.type}`);
+        }
+      };
 
-    if (this.audioContext === undefined) {
-      // setupAudioContext();
-    } 
+      try { // setup visual elements
+        this.canvas = document.getElementById(this.canvasID);
+        if (this.canvas) { 
+          this.renderer.setSize(window.innerWidth, window.innerHeight);
+          // remove any old renderers leftover from fast refresh
+          const old_renderer = document.getElementById('renderer');
+          if (old_renderer) {
+            old_renderer.parentNode.removeChild(old_renderer);
+          }
+          document.body.appendChild(this.renderer.domElement);  
 
-    if (this.gui === undefined) {
-      setupGeometry();
-      setupGui();
-      // await setupGui();
-    }  
+          // position camera directly above origin
+          this.camera.position.z = 150;
+          // point camera towards origin (see also: camera.lookat)
+          this.controls.target = new THREE.Vector3(0, 0, 0);
+        }
+      } catch (error) { console.error(error); }
 
-    window.addEventListener("resize", () => this.onWindowResize(), false);
+      try { // setup stats module
+        const stats = document.getElementById('stats');
+        if (stats) {
+          stats.parentNode.removeChild(stats)
+        }
+        this.stats = Stats();
+        this.stats.dom.id = 'stats';
+        this.mainDiv.appendChild(this.stats.dom);
+      } catch (error) { console.error(error); }
+
+      try { // setup oscilloscope module
+        const scope = document.getElementById('scope');
+        if (scope) {
+          scope.parentNode.removeChild(scope)
+        }
+        this.scope = new Oscilloscope();
+        this.scope.dom.id = 'scope';
+        this.mainDiv.appendChild(this.scope.dom);
+      } catch (error) { console.error(error); }
+
+      // try { // setup geometry and DAT GUI
+        if (this.gui === undefined) {
+          setupGeometry();
+          setupGui(); 
+        }  
+      // } catch (error) { console.error(error); }
+
+      // add event listeners
+      window.addEventListener("resize", () => this.onWindowResize(), { passive: true } );
+
+      const mediaEvents = ['loadstart', 'canplay', 'canplaythrough', 'play', 'pause'];
+      mediaEvents.forEach((event) => this.media.video.addEventListener(event, handleEvent));
+    };
+
+    setupDomElements();
   }
 
   animate() {
+    // call render function on every animation frame
+    // requestAnimationFrame(render);  
     window.requestAnimationFrame(this.animate.bind(this));
-    this.renderer.render(this.scene, this.camera);
-    // this.composer.render();
+
+    this.renderer.render(this.scene, this.camera); // this.composer.render();
     this.scope.render();
     this.stats.update();
     this.controls.update();
-  }
-
-  render() {
-    this.uniforms_fft.u_time.value += this.clock.getDelta();
-    // this.renderer.render(this.scene, this.camera);
-    this.composer.render();
-    this.scope.render();
   }
 
   onWindowResize() {
@@ -287,24 +371,54 @@ export default class SceneInit {
     this.scope.renderer.setSize(window.innerWidth, this.scope.audio.height);
   }
 
-  // send audio data to visual components
-  playMedia = async () => {
-    const render = (time) => {
-      // freeze waveform when song is paused
-      if (!this.audioElement.paused){
-        this.analyser.getByteFrequencyData(this.dataArray);
-        this.analyser.getFloatTimeDomainData(this.timeArray);
+}
 
-        this.uniforms_fft.u_time.value = time;
-        this.uniforms_fft.u_data_arr.value = this.dataArray;
-        this.uniforms_scope.u_data_arr.value = this.timeArray;
+// helper class for all the WebAudio API stuff
+class MediaInit { 
+  constructor(mediaID='media', fftSize=2048, mediaLink=mediaLinks[0]) {
+    this.mediaID = mediaID;
+    this.fftSize = fftSize;
+    this.mediaLink = mediaLink;
+    console.log('available tracks:')
+    // Object.entries(mediaLinks).forEach(([src, type]) => console.log(`${type} : ${src}`));
+    for (const [obj, track] of Object.entries(mediaLinks)) { 
+      for (const [attr, value] of Object.entries(track)) {
+        console.log(`${obj} : ${attr} : ${value}`)
       }
+    }
+    try { // create new media context every time the input track changes
+      // https://stackoverflow.com/questions/50657659/different-behaviour-of-webaudio-api-on-google-chrome  
+      this.mediaSource = document.createElement('source');
+      setMultiAttributes(this.mediaSource, {
+        'src':this.mediaLink['src'], 
+        'type':this.mediaLink['type'],
+      });
+      console.log("media source: " + this.mediaLink['src']);
 
-      // call render function on every animation frame
-      requestAnimationFrame(render);
-    };
+      this.video = document.createElement('video');
+      setMultiAttributes(this.video, {
+        'id':'media', 'controls':'', 'autoPlay':'', 'loop':true,  
+      });
+      this.video.appendChild(this.mediaSource);
+    } catch (error) { console.error(error); }
+  }
 
-    render();
-  };
+  setupAudioContext() {
+    try { // create new audio context and bind media element  to it
+      this.context = new window.AudioContext();
+      // can play audio/video from livestreams, but not access via script due to CORS policy: 
+      // need proxy server to override: https://medium.com/nodejsmadeeasy/a-simple-cors-proxy-for-javascript-applications-9b36a8d39c51
+      // "/proxy/rttp_stream.mp3" => "http://s9.viastreaming.net:9000/;stream.mp3"
+      this.elementSource = this.context.createMediaElementSource(this.video);
+    
+      // need separate gain node to adjust playback volume while preserving signal fidelity
+      this.gainVolume = new GainNode(this.context, { gain: 0.5 });
+      this.analyser = this.context.createAnalyser();
+      this.analyser.fftSize = this.fftSize;
+      this.elementSource.connect(this.analyser);
+      this.analyser.connect(this.context.destination);
 
+      console.log("media channels: " + this.analyser.channelCount);
+    } catch (error) { console.error(error); }
+  }
 }
